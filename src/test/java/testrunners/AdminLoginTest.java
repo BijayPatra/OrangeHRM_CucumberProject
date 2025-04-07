28a8026074f9006114f5e619973252689f0dd932
Bijay <vijaypatra2681@gmail.com>
2025-04-07 12:44:48 +0530

    lastcommit

--- src/test/java/testrunners/AdminLoginTest.java
+++ src/test/java/testrunners/AdminLoginTest.java
@@ -2,9 +2,7 @@
 package testrunners;
 
 import org.junit.runner.RunWith;
-
 import io.cucumber.junit.Cucumber;
-
 import io.cucumber.junit.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 //import io.cucumber.testng.CucumberOptions;
